a6a381e1abe0b2faf872fd8269a8a20716f59703
Qingshan Zhuan <qingshan.zhuan@fabricgroup.com.au>
2019-07-30 17:57:58 +1000

    update all commands

--- cli.py
+++ cli.py
@@ -31,38 +31,46 @@ def b():
 @click.command(short_help="Commit all the indexed files")
 @click.argument('comment', metavar='<comment>')
 def ci(comment):
-    """Commit all the indexed files into reposity, same as 'git commit -m'.
+    """Commit all the indexed files into reposity, same as 'git commit -m <comment>'.
     """
     Gity().ci(comment, None, None)
 
-@click.command(help="Add files into repository and commit")
-def cia():
-    pass
+@click.command(short_help="Add files into index and commit")
+@click.argument('comment', metavar='<comment>')
+@click.option('-x', '--exclude', metavar='<pathspec>')
+def cia(comment, exclude):
+    """Add files into index, and then commit to repository. \n
+    This is a combination of the following commands: \n
+    `git a . -x <pathspec>` \n
+    `git commit -m <comment>`
+    """
+    Gity().cia(comment, list(exclude))
 
 # parser.add_argument("branch", type=str, help=info("Branch name to checkout"))
 #     parser.add_argument("-b", action='store_true', help=info("Create a new branch if not existed"))
 
-co_help = info("""Similiar to [git checkout] \n
-This is not an alias of [git checkout]. \n
-In order to keep it simple, it only accepts two arguments: BRANCH,  and -b
-
-""") 
+co_help = """Check out branch <branch>, create if '-b' is True and the branch doesn't exist. \n
+This is similiar to 'git checkout', but doesn't support other parameters.
+"""
 @click.command( 
         help=co_help, 
-        short_help="Create new branch if it doesn't exist, same as the '-b' option in [git checkout]")
+        short_help="Check out branch <branch>, create if '-b' is True and the branch doesn't exist.")
 @click.option('-b', metavar='create_if_not_existed', required=False, 
                 is_flag=True, 
                 show_default=True,
-                help=info("Create new branch if it doesn't exist, same as the '-b' option in [git checkout]"))
+                help="Indicate to create the branch if it doesn't exist, same to '-B' option in 'git checkout' command.")
 @click.argument('branch', metavar='<branch>')
 @click.argument('start_point', metavar='<start_point>', required=False)
 def co(start_point, b, branch):
     print(branch, b, start_point)
     Gity().co(branch, start_point, b)
 
-@click.command(help="Show recent n logs(5 by default)")
-def llg():
-    pass
+@click.command(short_help="Show recent <number> logs(5 by default)",)
+@click.argument('n', metavar='<number>', default=5)
+def llg(n):
+    """Show recent <number> logs(5 by default), same to 'git log --oneline -n <number>'
+    """
+    Gity().llg()
 
 @click.command(help="Merge codes from branch <from>")
 @click.argument('from_', metavar='<from>')
@@ -73,17 +81,26 @@ def m(from_):
 def p():
     Gity().p()
 
-@click.command(help='Create pull request')
-def pr():
-    pass
+@click.command(short_help='Create pull request from current branch to <to_branch>')
+@click.argument('to_branch', metavar='<to_branch>', nargs=1)
+def pr(to_branch):
+    """Create pull request from current branch to <to_branch>.
+    Currently it only support to raise pull request to github.
+    The github repo url is retrieved from the .git/config file.
+    """
+    Gity().pr(to_branch)
 
-@click.command(help="Same as [git status]")
+@click.command(short_help="Show file status")
 def st():
+    """Show file status, same as 'git status'"""
     Gity().st()
 
-@click.command(help="create remote branch")
+@click.command(short_help="Create remote branch")
 def up():
-    pass
+    """Create remote branch, 
+    same as 'git push --set-upstream origin'
+    """
+    Gity().up()
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
